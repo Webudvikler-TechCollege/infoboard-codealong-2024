57143ba1ecf4f85deb9fd2b304d10b5207ba61f9
Webudvikler Hovedforløb <heka@techcollege.dk>
2024-04-04 12:36:16 +0200

    Initial Commit

--- assets/js/Utils/miscUtils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Capitalize the first letter of a string
- * @param {string} str - The string to capitalize
- * @returns {string} - The string with the first letter capitalized
- */
-export const capitalizeFirstLetter = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-};
