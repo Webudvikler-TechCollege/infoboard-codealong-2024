eafaada86174b294af2bcbecb518eef596bbc7b3
Webudvikler Hovedforløb <heka@techcollege.dk>
2024-04-09 13:22:04 +0200

    Events JS

--- assets/js/Components/Events.js
+++ assets/js/Components/Events.js
@@ -1,12 +1,33 @@
 import { myFetch } from "../Utils/apiUtils.js"
 
 export const Events = async () => {
+	const config = await myFetch('../../../config.json')
+
 	// Fetch event data from the API
 	const endpoint = "https://iws.itcn.dk/techcollege/schedules?departmentCode=smed"
-	const data = await myFetch(endpoint)
+	let { value: events_data } = await myFetch(endpoint)
 
 	const endpoint_friendly = "https://api.mediehuset.net/infoboard/subjects"
-	const friendly_data = await myFetch(endpoint_friendly)
+	const { result: friendly_data } = await myFetch(endpoint_friendly)
 
-	console.log(friendly_data);
+	events_data = events_data.filter(elm => config.array_valid_educations.includes(elm.Education))
+
+	events_data.map(event => {
+		event.Time = new Date(event.StartDate).toLocaleTimeString("en-GB", {
+			hour: "2-digit",
+			minute: "2-digit"
+		})
+
+		friendly_data.map(word => {
+			if(word.name.toUpperCase() === event.Education.toUpperCase()) {
+				event.Education = word.friendly_name
+			}
+			if(word.name.toUpperCase() === event.Subject.toUpperCase()) {
+				event.Subject = word.friendly_name
+			}
+		})
+		console.log(event);
+	})
+
+
 }
